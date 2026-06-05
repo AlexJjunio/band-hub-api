fd26a86cf33cced7cfa2d2e10b2ae7cddd68c9a6
AlexJjunio <alex.jjunio.adm@gmail.com>
2026-06-05 16:24:55 -0300

    chore: trigger redeploy to inspect startup logs

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-// bootstrap the application
+// bootstrap the application (redeploy trigger)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
